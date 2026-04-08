ed6353190ead77f417b9a82e798733e5e3302f66
Diego Rdz <a00835032@tec.mx>
2026-04-07 22:34:01 -0600

    final

--- Tarea1.py
+++ Tarea1.py
@@ -33,8 +33,7 @@ class Stack:
     def __repr__(self):
         return f"Stack({self._data})"
     
-
-
+    
 class Queue:
     def __init__(self):
         self._data = deque()
@@ -64,8 +63,6 @@ class Queue:
     def __repr__(self):
         return f"Queue({list(self._data)})"
     
-
-
 
 class OrderedHashTable:
     def __init__(self):
@@ -109,20 +106,79 @@ class OrderedHashTable:
         return f"OrderedHashTable({self._data})"
     
 
-# STACK
-s = Stack()
-s.push(10)
-s.push(20)
-print(s.pop())  
 
-# QUEUE
-q = Queue()
-q.enqueue(1)
-q.enqueue(2)
-print(q.dequeue())  
+#Pruebas
+def test_stack():
+    print("Stack pruebas")
+    s = Stack()
 
-# HASH TABLE
-h = OrderedHashTable()
-h.put("a", 100)
-h.put("b", 200)
-print(h.get("a"))  
+    assert s.is_empty()
+    s.push(10)
+    s.push(20)
+    assert s.size() == 2
+    assert s.peek() == 20
+    assert s.pop() == 20
+    assert s.pop() == 10
+    assert s.is_empty()
+
+    try:
+        s.pop()
+    except IndexError:
+        print("Stack error correctamente manejado")
+
+    print("Stack tests passed\n")
+
+
+def test_queue():
+    print("Pruebas Queu")
+    q = Queue()
+
+    assert q.is_empty()
+    q.enqueue(1)
+    q.enqueue(2)
+    assert q.size() == 2
+    assert q.peek() == 1
+    assert q.dequeue() == 1
+    assert q.dequeue() == 2
+    assert q.is_empty()
+
+    try:
+        q.dequeue()
+    except IndexError:
+        print("Queue error correctamente manejado")
+
+    print("Queue tests passed\n")
+
+def test_hash_table():
+    print("Pruebas Hash")
+    h = OrderedHashTable()
+
+    assert h.is_empty()
+    h.put("a", 100)
+    h.put("b", 200)
+
+    assert h.get("a") == 100
+    assert h.get("b") == 200
+
+    h.put("a", 300)
+    assert h.get("a") == 300
+
+    assert h.contains("a")
+    h.remove("a")
+    assert not h.contains("a")
+
+    assert h.keys() == ["b"]
+
+    try:
+        h.get("z")
+    except KeyError:
+        print("Hash error correctamente manejado")
+
+    print("Hash Table tests passed\n")
+
+
+if __name__ == "__main__":
+    test_stack()
+    test_queue()
+    test_hash_table()
+    print("pruebas exito")
